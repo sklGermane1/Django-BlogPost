7ae84b66a03401a7a8b2e285f29e2c6a5950d55e
Bill <billcrafter007@gmail.com>
2020-11-10 18:47:07 +0100

    Created Registration

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 # Create your views here.
 
 def register(request):
-    context = {}
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request,f"Account created for {username}!")
+            return redirect("posts-home")
+
+    else:
+        form = UserRegisterForm()
+    context = {
+            "form":form
+        }
     return render(request,"users/register.html",context)
